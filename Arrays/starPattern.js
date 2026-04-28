91c62b182a2f125ba8104c132afb29ddf61a3525
Jeet Thakur <jeetthakur251@gmail.com>
2026-04-28 07:30:37 +0530

    star pattern new variation

--- Arrays/starPattern.js
+++ Arrays/starPattern.js
@@ -32,10 +32,34 @@
 // 123
 // 1234
 
-for(let i = 1; i <5;i++){
-    let row = "";
-    for(let j = 1 ; j <=i ;j++){
-        row = row + j
-    }
-    console.log(row)
-}
+// for(let i = 1; i <5;i++){
+//     let row = "";
+//     for(let j = 1 ; j <=i ;j++){
+//         row = row + j
+//     }
+//     console.log(row)
+// }
+
+
+
+
+// 1
+// 22
+// 333
+// 4444
+
+// for(let i=0;i<5;i++){
+//     let row = "";
+//     for(let j=0;j<=i;j++){
+//         row = row+(i+1)
+//     }
+//     console.log(row)
+// }
+
+// for(let i=1;i<5;i++){
+//     let row = "";
+//     for(let j=1;j<=i;j++){
+//         row = row+(i)
+//     }
+//     console.log(row)
+// }
