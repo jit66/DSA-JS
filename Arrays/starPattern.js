7f9f4fcefa7c4663573a161cc58f0929a58cc7f3
Jeet Thakur <jeetthakur251@gmail.com>
2026-05-02 00:01:47 +0530

    star pattern completed

--- Arrays/starPattern.js
+++ Arrays/starPattern.js
@@ -80,22 +80,77 @@
 //     console.log(row)
 // }
 
+
+
 //     *
 //    **
 //   ***
 //  ****
 // *****
 
-let n=5
-for(let i = 0;i<n;i++){
+// let n=5
+// for(let i = 0;i<n;i++){
+//     let row = "";
+//     // for(let j=0 ; j<n-(i+1);j++){
+//     for(let j=0 ; j<n-i;j++){
+//         row = row + "-"
+//     }
+//     for(let k = 0 ; k < i+1;k++){
+//         row = row + "*"
+//     }
+//     console.log(row)
+// }
+
+
+
+
+// 1
+// 10
+// 101
+// 1010
+// 10101
+// 101010
+
+// for(let i=0 ; i<5;i++){
+//     let row = "";
+//     let toggle = 1;
+    
+//     for(let j=0; j<=i;j++){
+//         row = row + toggle;
+//         if(toggle==1){
+//             toggle=0
+//         }
+//         else{
+//             toggle=1
+//         }
+//     }
+//     console.log(row)
+// }
+
+
+
+
+// 1
+// 01
+// 010
+// 1010
+// 10101
+// 010101
+
+let n = 6;
+
+let toggle = 1;
+for(let i=0;i<n;i++){
     let row = "";
-    // for(let j=0 ; j<n-(i+1);j++){
-    for(let j=0 ; j<n-i;j++){
-        row = row + "-"
-    }
-    for(let k = 0 ; k < i+1;k++){
-        row = row + "*"
+
+    for(let j=0;j<i+1;j++){
+        row = row + toggle;
+        if(toggle == 1){
+            toggle = 0
+        }
+        else{
+            toggle = 1
+        }
     }
     console.log(row)
 }
-
