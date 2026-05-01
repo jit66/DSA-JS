508fc16974689395e930c3aca4363b0049ef6d01
Jeet Thakur <47236421+jit66@users.noreply.github.com>
2026-05-01 12:17:59 +0000

    new pattern updated

--- Arrays/starPattern.js
+++ Arrays/starPattern.js
@@ -71,11 +71,31 @@
 // 12
 // 1
 
-let n = 5
-for(let i = 0 ; i<n ; i++){
+// let n = 5
+// for(let i = 0 ; i<n ; i++){
+//     let row = "";
+//     for(let j=0;j<n-i;j++){
+//         row = row + (j+1)
+//     }
+//     console.log(row)
+// }
+
+//     *
+//    **
+//   ***
+//  ****
+// *****
+
+let n=5
+for(let i = 0;i<n;i++){
     let row = "";
-    for(let j=0;j<n-i;j++){
-        row = row + (j+1)
+    // for(let j=0 ; j<n-(i+1);j++){
+    for(let j=0 ; j<n-i;j++){
+        row = row + "-"
+    }
+    for(let k = 0 ; k < i+1;k++){
+        row = row + "*"
     }
     console.log(row)
 }
+
